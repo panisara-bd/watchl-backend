ccbcab83accdde375a2f8b0746a0dd01d1dd0aab
panisara-bd <panisara.bnn@gmail.com>
2023-04-24 13:50:17 +0200

    Configure for Lambda implimentation

---
+++ src/handlers/search-media.ts
@@ -0,0 +1,1 @@
+export const handler = (): string => 'Search jepw';
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,40 @@
 const path = require('path');
+const ZipPlugin = require('zip-webpack-plugin');
+
+const handlers = ['get-media', 'search-media'];
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: handlers.reduce(
+    (acc, handler) => ({
+      ...acc,
+      [handler]: `./src/handlers/${handler}.ts`,
+    }),
+    {}
+  ),
+  mode: 'production',
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs',
+  },
+  plugins: handlers.map(
+    (handler) =>
+      new ZipPlugin({
+        filename: `${handler}.zip`,
+        include: [`${handler}.js`],
+      })
+  ),
 };
